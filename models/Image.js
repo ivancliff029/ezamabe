dbc5d95324f1009834322e39857606a15138f01f
ivancliff029 <odekeivancliff@gmail.com>
2025-12-26 17:11:39 +0300

    Update:added category to image model

--- models/Image.js
+++ models/Image.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const imageSchema = new mongoose.Schema({
   title: String,
   url: String,      // Cloudinary or local link
+  category: String,
   createdAt: { type: Date, default: Date.now }
 });
 
